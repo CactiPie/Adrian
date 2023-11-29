7de1d27943a834a96e349dc093072ee7b366686d
CactiPie <152335164+CactiPie@users.noreply.github.com>
2023-11-29 14:12:47 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+num = random.randint(1, 25)
+guesss = 0
+while True:
+  print('guess a number between 1 and 25:')
+  guess = int(input())
+  if guess > num:
+    print('Guess lower.')
+  elif num > guess:
+    print('Guess higher.')
+  else:
+    break
+print('You got it! it was', num)
